100df9e03f688c29c5e02021613c4672ccb61181
Nokorbis <nokorbis@gmail.com>
2022-09-13 23:06:26 +0200

    Initial commit

---
+++ src/main/java/fr/bendertales/mc/taleswarper/TalesWarper.java
@@ -0,0 +1,11 @@
+package fr.bendertales.mc.taleswarper;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class TalesWarper implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
